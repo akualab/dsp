274045c595a05480d907e6fd3a880e6a906ab96f
Leo Neumeyer <leoneu@apache.org>
2015-05-26 15:44:32 -0700

    Add examples and docs.

--- doc.go
+++ doc.go
@@ -12,37 +12,32 @@ is associated with a disctrete time. Discrete time is represented as a
 squence of integers that can correspond to physycal time sampled at fixed
 time intervals.
 
-All operations are synchronous because all values must correspond to a discrete
-time. For example, given and operation such as x[n] = f[n-1] + g[n-2], the
-value x[4] is calculated using delayed values f[3] and g[2].
+Sequences are divided into equally-spaced frames such that each frame corresponds
+to a fixed number of samples.
 
-Tn application is a graph of processors where the input of a processor is read
-from the output of another processor. Each data object moving out of a processor
-is associated with a frame index. The data frame rate depends on the processors
-which may interpolate or decimate the input data.
+Processors can return values for a frame index or a global value for the entire sequence.
+To compute frame-level values, the processor must implement the Framer interface
+which defines the "Get(int) (Value, error)" method. To return a global value the processor must
+implement the OneValuer interface which defines the "Get() (Value, error.)" method.
 
-The architecture uses a "pull" approach. That is, when a frame is requested for
+The application is a graph of processors where the input of a processor is read
+from the output of another processor. Processors exchange values of interface type Value. To perform
+operations, the Processors must agree on the underlying types of Value.
+
+Values are lazily computed when they are requested by a consumer. That is, when a value is requested for
 one of the processor's outputs, the computation chain propagates all the way to
 the source.
 
-Computed frames are cached by the processors to avoid recomputing the saem frame
-more than once. This is important when a frame is requested multiple times by various
-processors or by a single processor. (For example, to do a moving average, the same
-input frames may be requested multiple times.)
-
-
-A more realistic application will have processors with multiple
-inputs and multiple outputs. The Builder functions provide a set of
-tools to create the application graph.
+To achieve high performance, computed frames are cached by the processors. If the cache capacity is big enough,
+values are only computed once. (For example, to do a moving average, the same input frames may be
+requested multiple times.)
 
 For a comrehensive example see examples/speech2/main.go.
 
 Convention: Input values should be treated as read-only because
 they may be shared with other processors.
-To quickly create a copy, do newValue = inputValue.Copy().
 
 CONTACT:
-
 Leo Neumeyer
 leo@akualab.com
 
--- example_test.go
+++ example_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/akualab/dsp"
 	"github.com/akualab/dsp/wav"
+
+	narray "github.com/akualab/narray/na64"
 )
 
 func ExampleSpectrum() {
@@ -51,4 +53,74 @@ func ExampleSpectrum() {
 	fmt.Println(v.Data[0])
 	// Output:
 	// 5.123420120893221e-05
+}
+
+// Calculate Fibonacci values.
+func ExampleFibonacci() {
+	app := dsp.NewApp("Fibonacci")
+	fibo := app.Add("Fibo", Fibo(11))
+
+	// Self loop.
+	app.Connect(fibo, fibo)
+
+	f10, e := fibo.Get(10)
+	if e != nil {
+		panic(e)
+	}
+	fmt.Println(f10.Data[0])
+	// Output:
+	// 89
+}
+
+// Subtract a value "fm" from a Fibonacci series so the sum adds up to zero.
+func ExampleZMFibonacci() {
+	app := dsp.NewApp("Fibonacci")
+	fibo := app.Add("Fibo", Fibo(11))
+
+	// Computes the mean of the series.
+	fiboMean := app.Add("Fibo Mean", dsp.Mean())
+
+	// Subtracts mean from fibo values.
+	fiboZM := app.Add("ZM Fibo", dsp.Sub())
+
+	// Make connections. Note that fiboMean is of tyep OneValuer and fibo is a Framer.
+	// The reulting fiboZM is of type Framer. Values are computed only once and saved
+	// in the processor cache.
+	app.Connect(fibo, fibo)
+	app.Connect(fiboMean, fibo)
+	app.Connect(fiboZM, fibo, fiboMean)
+
+	f10, e := fiboZM.Get(10)
+	if e != nil {
+		panic(e)
+	}
+	fmt.Println(f10.Data[0])
+	// Output:
+	// 67.9090909090909
+}
+
+// Fibo is a processor that returns the Fibonacci value for index.
+// N is the length of the series.
+func Fibo(N int) dsp.Processer {
+	return dsp.NewProc(20, func(idx int, in ...dsp.Processer) (dsp.Value, error) {
+		if idx == 0 || idx == 1 {
+			na := narray.New(1)
+			na.Set(1, 0)
+			return na, nil
+		}
+		if idx < 0 || idx >= N {
+			return nil, dsp.ErrOOB
+		}
+		vm1, err := dsp.Processers(in).Get(idx - 1)
+		if err != nil {
+			return nil, err
+		}
+		vm2, err := dsp.Processers(in).Get(idx - 2)
+		if err != nil {
+			return nil, err
+		}
+
+		res := narray.Add(nil, vm2, vm1)
+		return res, nil
+	})
 }
